9ed285df8c1f8213ee425a7721f96f1622cee284
MislavS <mislav.serdar1@gmail.com>
2024-06-07 17:27:06 +0200

    Dovrsen program
    Base program je dovrsen. Sve logike rade te izbor Da/Ne. Jos dodati bin i txt datoteke te leaderboard!

--- SPOJI432/spoji4-nadamse.cpp
+++ SPOJI432/spoji4-nadamse.cpp
@@ -14,7 +14,6 @@ void ocistiTerminal()
     cout << "\033[2J\033[1;1H";
 }
 
-
 void printanje_ploce() // https://stackoverflow.com/questions/22290174/connect-four-game-board-in-c
 {
     for (int i = 0; i < RED; i++)
@@ -28,9 +27,9 @@ void printanje_ploce() // https://stackoverflow.com/questions/22290174/connect-f
         cout << "|";
         for (int j = 0; j < STUPAC; j++)
         {
-            if(ploca[i][j] == 0)
+            if (ploca[i][j] == 0)
                 cout << "   |";
-            else if (ploca [i][j] == 1)
+            else if (ploca[i][j] == 1)
                 cout << " X |";
             else
                 cout << " 0 |";
@@ -142,7 +141,69 @@ bool ubaciZeton(int ploca[RED][STUPAC], char zeton, int stupac)
         }
     }
     return false; // ako je stupac pun vraca false
-} 
+}
+
+void igra()
+{
+    string igrac1,
+        igrac2;
+    upisImena(igrac1, igrac2);
+    while (true)
+    {
+        for (int i = 0; i < RED; i++)
+        {
+            for (int j = 0; j < STUPAC; j++)
+            {
+                ploca[i][j] = 0;
+            }
+        }
+
+        int trenutniIgrac = 1;
+        char zeton;
+
+        while (true)
+        {
+            // clearScreen();
+            ocistiTerminal();
+            printanje_ploce();
+
+            zeton = (trenutniIgrac == 1) ? 1 : 2;
+            cout << (trenutniIgrac == 1 ? igrac1 : igrac2) << " je na potezu. Njegov zeton je: " << (zeton == 1 ? 'X' : 'O') << endl;
+
+            int stupac = odabirStupca(ploca);
+            ubaciZeton(ploca, zeton, stupac);
+
+            if (provjera_Pobjednik(ploca, zeton))
+            {
+                // clearScreen();
+                ocistiTerminal();
+                printanje_ploce();
+                cout << (trenutniIgrac == 1 ? igrac1 : igrac2) << " je pobjedio!" << endl;
+                break;
+            }
+            if (provjera_PunaPloca(ploca))
+            {
+                // clearScreen();
+                ocistiTerminal();
+                printanje_ploce();
+                cout << "Ploca je popunjena! Nerjeseno!! " << endl;
+                break;
+            }
+            trenutniIgrac = (trenutniIgrac == 1) ? 2 : 1;
+        }
+
+        string odgovor;
+        cout << "Zelite li igrati ponovo? (da/ne): ";
+        cin >> odgovor;
+        cin.ignore();
+        if (odgovor != "da" && odgovor != "Da" && odgovor != "DA")
+        {
+            // igra();
+            ocistiTerminal();
+            break;
+        }
+    }
+}
 
 int main()
 {
@@ -164,68 +225,20 @@ int main()
 
         if (izbor == 1)
         {
-            string igrac1, igrac2;
-            upisImena(igrac1, igrac2);
-
-            for (int i = 0; i < RED; i++)
-            {
-                for (int j = 0; j < STUPAC; j++)
-                {
-                    ploca[i][j] = 0;
-                }
-            }
-            int trenutniIgrac = 1;
-            char zeton;
-
-            while (true)
-            {
-                //clearScreen();
-                ocistiTerminal();
-                printanje_ploce();
-
-                zeton = (trenutniIgrac == 1) ? 1 : 2;
-                cout << (trenutniIgrac == 1 ? igrac1 : igrac2) << " je na potezu. Njegov zeton je: " << (zeton == 1 ? 'X' : 'O') << endl;
-
-                int stupac = odabirStupca(ploca);
-                ubaciZeton(ploca, zeton, stupac);
-
-                if (provjera_Pobjednik(ploca, zeton))
-                {
-                    //clearScreen();
-                    ocistiTerminal();
-                    printanje_ploce();
-                    cout << (trenutniIgrac == 1 ? igrac1 : igrac2) << " je pobjedio!" << endl;
-                    break;
-                }
-                if (provjera_PunaPloca(ploca))
-                {
-                    //clearScreen();
-                    ocistiTerminal();
-                    printanje_ploce();
-                    cout << "Ploca je popunjena! Nerjeseno!! " << endl;
-                    break;
-                }
-                trenutniIgrac = (trenutniIgrac == 1) ? 2 : 1;
-            }
-
-            string odgovor;
-            cout << "Zelite li igrati ponovo? (da/ne): ";
-            cin >> odgovor;
-            if(odgovor != "da" && odgovor != "Da" && odgovor != "DA")
-            {
-                break;
-            }
+            cout << "Zapoceli ste igru!" << endl;
+            igra();
         }
 
-        if (izbor == 2)
+        else if (izbor == 2)
         {
             cout << "Leaderboard jos nije gotov!"; // uzima imena i bodove iz tekstualne datoteke/binarne
         }
-        if (izbor == 3)
+        else if (izbor == 3)
         {
+            cout << "Odabrali ste izlazak iz igrice! Doviđenja! " << endl;
             break;
         }
-        else 
+        else
         {
             cout << "Odabrali ste izbor koji ne postoji! Molim unesite validan izbor! " << endl;
         }
